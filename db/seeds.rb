4bb99595445ee04f2117f42e0e77db43cabb9bbc
Mike Lerch <mike.lerch@gmx.de>
2012-05-11 18:14:13 +0200

    Added some sample data.
    Type 'rake db:seed' to use seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+u1 = User.create(name: 'Fritz')
+u2 = User.create(name: 'Heike')
+u3 = User.create(name: 'Alfons')
+u4 = User.create(name: 'Kriemhild')
+u5 = User.create(name: 'Charles')
+u6 = User.create(name: 'Diana')
+
+Topic.delete_all
+Topic.create(title: 'RESTful Web Services', description: 'Lore ipsum ..', user_id: u1.id)
+Topic.create(title: 'How to avoid bugs', description: 'Isds sd sd sds ', user_id: u1.id)
+Topic.create(title: 'I am the greatest', description: 'bla bla bla bla', user_id: u4.id)
+

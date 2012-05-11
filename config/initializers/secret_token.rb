c72932c21508e8ae44a456aeb1b9898849bfd44c
Mike Lerch <mike.lerch@gmx.de>
2012-05-11 17:30:57 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techtalk::Application.config.secret_token = '11ef3236e3ecd0295b262f9e543526142d49d46b5705c901654c93fd269553e79c22c794600fade6699aaf22b0076d8cdb43666ce2c70e7edc1525ecaa4a12b3'

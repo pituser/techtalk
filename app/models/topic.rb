bcf489da1f211b175bfe486cea98804744d55349
Mike Lerch <mike.lerch@gmx.de>
2012-05-11 17:59:43 +0200

    Added 1:n relation between User and Topic.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,5 @@
 class Topic < ActiveRecord::Base
   attr_accessible :description, :name, :user_id
+  
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :name
+  
+  has_many :topics, dependent: :destroy
 end

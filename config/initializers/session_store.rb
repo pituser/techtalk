a646ef13b85b3d3b62ace5294fc552489b6c500e
Mike Lerch <mike.lerch@gmx.de>
2012-05-13 16:24:18 +0200

    Removed Session controller (doesn't follow pluralized name convention).

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Techtalk::Application.config.session_store :cookie_store, key: '_techtalk_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
